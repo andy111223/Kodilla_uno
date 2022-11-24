90ee0df3fd1b5707c2f5e07a710cb6237dd4aab7
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2022-11-24 22:13:56 +0100

    7.1 Beautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+
+    public void beautify(String textToDecorate, PoemDecorator poemDecorator){
+
+        String result = poemDecorator.decorate(textToDecorate);
+        System.out.println("Decorated text: " + result);
+
+    }
+}
