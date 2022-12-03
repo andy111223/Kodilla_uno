62af024090941f15e2d7a349c91d5f1b24175116
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2022-12-03 21:20:24 +0100

    FindFlight 2

--- kodilla-exception/src/main/java/com/kodilla/Main.java
+++ kodilla-exception/src/main/java/com/kodilla/Main.java
@@ -1,7 +1,25 @@
 package com.kodilla;
 
+import com.kodilla.exception.test.Flight;
+import com.kodilla.exception.test.FindFlight;
+import com.kodilla.exception.test.RouteNotFoundException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+     public static void main(String[] args) {
+
+         Flight flight = new Flight("Atlanta", "Boston");
+         FindFlight findFlight = new FindFlight();
+
+
+         try {
+             findFlight.findFlight(flight);
+         } catch (RouteNotFoundException e) {
+             System.out.println("No such airport");
+         }
+         System.out.println("The end of search");
+     }
 }
