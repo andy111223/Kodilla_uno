5cfd8f5474790ae048792822babf1d197fc335de
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2022-12-06 03:13:30 +0100

    8.4 RouteNotFoundException

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -1,21 +1,23 @@
 package com.kodilla.exception.test;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class FindFlight {
 
-    Map<String, Boolean> flightsMap = new HashMap<>();
 
-    public Map<String, Boolean> findFlight (Flight flight) throws RouteNotFoundException {
+    public boolean findFlight (Flight flight) throws RouteNotFoundException {
 
-        try (flightsMap.get()) {
-            return flightsMap.entrySet();
-        } catch (KeyNotFoundException e){
+        Map<String, Boolean> flightsMap = new HashMap<>();
+
+        try {
+            if (flightsMap.containsKey(flight.getArrivalAirport())) {
+                return flightsMap.containsKey(flight.getArrivalAirport());
+            }
+        } catch (Exception e){
             throw new RouteNotFoundException("Airport not found.");
         }
         System.out.println("Aiport search finished.");
+        return false;
     }
 }
