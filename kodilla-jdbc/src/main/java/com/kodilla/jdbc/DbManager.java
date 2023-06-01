1374379e1862e076aa6595a6ca0ca2ee261b07c8
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2023-06-01 22:15:05 +0200

    16.6 Aktywni użytkownicy

---
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -0,0 +1,33 @@
+package com.kodilla.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public enum DbManager {
+
+    INSTANCE;
+
+    private Connection conn;
+
+    DbManager() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user","kodilla_user");
+        connectionProps.put("password", "kodilla_Pass123");
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/KODILLA_COURSE?serverTimezone=Europe/Warsaw&useSSL=False", connectionProps
+            );
+        } catch (SQLException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static DbManager getInstance() {
+        return INSTANCE;
+    }
+    public Connection getConnection() {
+        return conn;
+    }
+}
